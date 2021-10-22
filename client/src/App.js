9780190bf8bac736bf5c2ff2f81903cc948d1954
Ashu <ashu@gmail.com>
2021-10-22 20:23:38 +0530

    change cors url and add package-lock.json file

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import React, { useEffect, useState, useRef } from 'react';
 import io from "socket.io-client";
 import Peer from "simple-peer";
 
-const socket = io.connect("http://localhost:5000");
+const socket = io.connect("https://ashreactvideoapp.herokuapp.com");
 
 
 function App() {
