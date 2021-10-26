e1cabf757bcb04784b27467649d0b787c291dbd5
Ashu <ashu@gmail.com>
2021-10-26 20:03:08 +0530

    added axios in frontend

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,21 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from "axios";
 import Navbar from '../components/Navbar';
 import "../css/login.css";
 
 const Login = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const getEmail = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const getPassword = (e) => {
+        setPassword(e.target.value);
+    }
+
+    // const data = { email, password };
+
+    const submitForm = (e) => {
+        e.preventDefault();
+        
+        axios.get("http://localhost:5000/")
+            .then((response) => {
+                console.log(response.status);
+            }).catch((err) => {
+                console.log(err);
+            });
+    }
+
+
+
+
+
+
+
     return (
         <>
             <Navbar />
             <div className="form">
-                <form action="/login" method="POST" className="login-form">
+                <form className="login-form">
                     <i className="fas fa-user-circle"></i>
-                    <input type="email" className="user-input" placeholder="Email" name="email" required />
+                    <input type="email" className="user-input" placeholder="Email" name="email" required value={email} onChange={getEmail} />
                     <input type="password" className="user-input" placeholder="Password" name="password"
-                        required />
+                        required value={password} onChange={getPassword} />
                     {/* <div className="option-1">
                                 <a href="#">Forgot Password</a>
                             </div> */}
-                    <button type="submit" className="btn">Login</button>
+                    <button type="submit" className="btn" onClick={submitForm}>Login</button>
                     <div className="option-2">
                         <p>Not Registered ? <a href="/signup">Create an Account</a></p>
                     </div>
